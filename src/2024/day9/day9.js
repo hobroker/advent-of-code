5a84a4c8580267c40c116db0b15e5074de060e84
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2024-12-11 14:57:14 +0200

    2024 day 9 part 2

--- src/2024/day9/day9.js
+++ src/2024/day9/day9.js
@@ -1,16 +1,30 @@
 import { read } from '../../../lib/read.js';
-import { addIndex, chain, compose, map, reduce, split } from 'ramda';
+import {
+  addIndex,
+  chain,
+  compose,
+  flatten,
+  identity,
+  map,
+  reduce,
+  split,
+} from 'ramda';
 
 const data = read('input.txt');
 
-const prepare = compose(map(Number), split(''));
+const prepare = fn =>
+  compose(
+    addIndex(fn)((item, index) =>
+      Array(item).fill(index % 2 ? '.' : `${index / 2}`),
+    ),
+    map(Number),
+    split(''),
+  );
 
 const checksum = addIndex(reduce)(
   (acc, item, index) => acc + (isNaN(item) ? 0 : item) * index,
   0,
 );
-const setupBlocks = (item, index) =>
-  Array(item).fill(index % 2 ? '.' : index / 2);
 
 const lastNonDotElement = list => {
   while (list.length > 0) {
@@ -22,11 +36,36 @@ const lastNonDotElement = list => {
 const part1 = compose(
   checksum,
   list => list.flatMap(item => [item !== '.' ? item : lastNonDotElement(list)]),
-  addIndex(chain)(setupBlocks),
-  prepare,
+  prepare(chain),
 );
 
-// const part2 = compose(prepare);
+const part2 = compose(
+  checksum,
+  flatten,
+  disk => {
+    const diskCopy = disk.map(map(identity));
+    for (let i = disk.length - 1; i >= 0; i--) {
+      if (disk[i][0] === '.') continue;
+      const block = disk[i];
+      const [item] = block;
+      for (let j = 0; j < diskCopy.length && item !== diskCopy[j][0]; j++) {
+        const size = diskCopy[j].length - block.length;
+        if (diskCopy[j][0] !== '.' || size < 0) continue;
+        const nextIndex = diskCopy.findIndex(([file]) => file === item);
+        diskCopy[nextIndex] = diskCopy[nextIndex].fill('.');
+        diskCopy.splice(
+          j,
+          1,
+          block,
+          ...(size > 0 ? [diskCopy[j].slice(0, size)] : []),
+        );
+        break;
+      }
+    }
+    return diskCopy;
+  },
+  prepare(map),
+);
 
 console.log('part 1', part1(data));
-// console.log('part 2', part2(data));
+console.log('part 2', part2(data));
