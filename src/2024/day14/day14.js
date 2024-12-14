ec4e48b9552653ffbf2b002db2e5ac5f95cf1fef
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2024-12-14 16:31:10 +0200

    2024 day 14 part 2

--- src/2024/day14/day14.js
+++ src/2024/day14/day14.js
@@ -4,19 +4,34 @@ import {
   compose,
   equals,
   identity,
+  juxt,
   last,
   length,
   map,
   multiply,
   reduce,
+  slice,
   split,
   sum,
-  tap,
+  times,
 } from 'ramda';
 
 const data = read('input.txt');
 
+const timesN = times(identity);
+
+const buildSpace = (x, y) => robots => {
+  const matrix = [];
+  timesN(x).forEach(i => timesN(y).forEach(j => ((matrix[i] ||= [])[j] = [])));
+  robots.forEach(robot => matrix[robot[0][1]][robot[0][0]].push(robot));
+  return matrix;
+};
+
 const prepare = compose(
+  applySpec({
+    robots: identity,
+    matrix: buildSpace(103, 101),
+  }),
   map(
     compose(
       map(compose(map(Number), split(','), last, split('='))),
@@ -26,55 +41,37 @@ const prepare = compose(
   split('\n'),
 );
 
-const buildSpace = (x, y) => robots => {
-  const matrix = [];
-  for (let i = 0; i < x; i++) {
-    matrix[i] = [];
-    for (let j = 0; j < y; j++) {
-      matrix[i][j] = [];
-    }
-  }
-  robots.forEach(robot => matrix[robot[0][1]][robot[0][0]].push(robot));
-  return matrix;
+const logMatrix = matrix =>
+  console.log(
+    matrix
+      .map(row => row.map(cell => (cell.length ? '#' : '.')).join(''))
+      .join('\n'),
+  );
+
+const splitToQuadrants = matrix => {
+  const height = matrix.length / 2;
+  const width = matrix[0].length / 2;
+  return [matrix.slice(0, height), matrix.slice(height + 1)].flatMap(
+    juxt([map(slice(0, width)), map(slice(width + 1, width * 2))]),
+  );
 };
 
-const logMatrix = tap(matrix => {
-  matrix.forEach(row => {
-    console.log(row.map(cell => cell.length || '.').join(''));
+const tick = ({ robots, matrix }) => {
+  robots.forEach(robot => {
+    const [[positionY, positionX], [velocityY, velocityX]] = robot;
+    const cell = matrix[positionX][positionY];
+    cell.splice(cell.findIndex(equals(robot)), 1);
+    let nextX = (positionX + velocityX + matrix.length) % matrix.length;
+    let nextY = (positionY + velocityY + matrix[0].length) % matrix[0].length;
+    matrix[nextX][nextY].push(robot);
+    robot[0] = [nextY, nextX];
   });
-});
-
-const splitToQuadrants = matrix => [
-  matrix
-    .slice(0, Math.floor(matrix.length / 2))
-    .map(row => row.slice(0, Math.floor(row.length / 2))),
-  matrix
-    .slice(0, Math.floor(matrix.length / 2))
-    .map(row => row.slice(Math.ceil(row.length / 2))),
-  matrix
-    .slice(Math.ceil(matrix.length / 2))
-    .map(row => row.slice(0, row.length / 2)),
-  matrix
-    .slice(Math.ceil(matrix.length / 2))
-    .map(row => row.slice(Math.ceil(row.length / 2))),
-];
+};
 
 const loop =
   n =>
   ({ robots, matrix }) => {
-    for (let i = 0; i < n; i++) {
-      robots.forEach(robot => {
-        const [[positionY, positionX], [velocityY, velocityX]] = robot;
-        const cell = matrix[positionX][positionY];
-        cell.splice(cell.findIndex(equals(robot)), 1);
-        let nextX = (positionX + velocityX + matrix.length) % matrix.length;
-        let nextY =
-          (positionY + velocityY + matrix[0].length) % matrix[0].length;
-
-        matrix[nextX][nextY].push(robot);
-        robot[0] = [nextY, nextX];
-      });
-    }
+    timesN(n).forEach(() => tick({ robots, matrix }));
     return matrix;
   };
 
@@ -83,15 +80,29 @@ const part1 = compose(
   map(compose(sum, map(compose(sum, map(length))))),
   splitToQuadrants,
   loop(100),
-  applySpec({
-    // matrix: buildSpace(103, 101),
-    matrix: buildSpace(7, 11),
-    robots: identity,
-  }),
   prepare,
 );
 
-// const part2 = compose(prepare);
+const findTreeSecond = ({ matrix, robots }) => {
+  let second = 0;
+  while (true) {
+    second++;
+    tick({ robots, matrix });
+    for (const row of matrix) {
+      if (
+        row
+          .map(cell => (cell.length ? '#' : '.'))
+          .join('')
+          .includes('###############')
+      ) {
+        logMatrix(matrix);
+        return second;
+      }
+    }
+  }
+};
+
+const part2 = compose(findTreeSecond, prepare);
 
 console.log('part 1', part1(data));
-// console.log('part 2', part2(data));
+console.log('part 2', part2(data));
