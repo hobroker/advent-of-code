facddd3d25b135d8286e0e5110765958fda361db
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2024-12-15 21:13:52 +0200

    2024 day 15 part 2

--- src/2024/day15/day15.js
+++ src/2024/day15/day15.js
@@ -1,5 +1,24 @@
 import { read } from '../../../lib/read.js';
-import { applySpec, chain, compose, head, last, map, split } from 'ramda';
+import {
+  addIndex,
+  always,
+  applySpec,
+  chain,
+  compose,
+  cond,
+  equals,
+  evolve,
+  head,
+  identity,
+  juxt,
+  last,
+  map,
+  split,
+  sum,
+  T,
+} from 'ramda';
+
+const chainIndexed = addIndex(chain);
 
 const data = read('input.txt');
 
@@ -18,53 +37,109 @@ const directions = {
   v: [1, 0],
 };
 
-const move = ({ matrix, direction: [dx, dy] }) => {
+const calculate = char =>
+  compose(
+    sum,
+    chainIndexed((row, i) =>
+      row.map((cell, j) => cell === char && 100 * i + j),
+    ),
+  );
+
+const execute = (matrix, moves) => {
+  moves.forEach(({ from, to }) => {
+    [matrix[from[0]][from[1]], matrix[to[0]][to[1]]] = [
+      matrix[to[0]][to[1]],
+      matrix[from[0]][from[1]],
+    ];
+  });
+};
+
+const move = (matrix, [dx, dy]) => {
   const positionX = matrix.findIndex(row => row.includes('@'));
   const positionY = matrix[positionX].indexOf('@');
   const nextX = positionX + dx;
   const nextY = positionY + dy;
-  if (matrix[nextX]?.[nextY] === '#') return;
-  if (matrix[nextX]?.[nextY] === 'O') {
-    const boxesToMove = [
-      { from: [nextX, nextY], to: [nextX + dx, nextY + dy] },
-    ];
-    if (matrix[boxesToMove[0].to[0]]?.[boxesToMove[0].to[1]] === '#') return;
-    while (matrix[boxesToMove[0].to[0]]?.[boxesToMove[0].to[1]] === 'O') {
-      const [{ from, to }] = boxesToMove;
-      if (matrix[to[0] + dx][to[1] + dy] === '#') return;
-      boxesToMove.unshift({
+  if (matrix[nextX][nextY] === '#') return;
+  if (matrix[nextX][nextY] === 'O') {
+    const moves = [{ from: [nextX, nextY], to: [nextX + dx, nextY + dy] }];
+    while (matrix[moves[0].to[0]][moves[0].to[1]] === 'O') {
+      const [{ from, to }] = moves;
+      moves.unshift({
         from: [from[0] + dx, from[1] + dy],
         to: [to[0] + dx, to[1] + dy],
       });
     }
-    boxesToMove.forEach(({ from, to }) => {
-      matrix[from[0]][from[1]] = '.';
-      matrix[to[0]][to[1]] = 'O';
-    });
+    if (moves.some(({ to: [x, y] }) => matrix[x][y] === '#')) return;
+    execute(matrix, moves);
   }
-  matrix[positionX][positionY] = '.';
-  matrix[nextX][nextY] = '@';
+  execute(matrix, [{ from: [positionX, positionY], to: [nextX, nextY] }]);
 };
 
-const part1 = compose(
-  matrix =>
-    matrix.reduce(
-      (acc, row, i) =>
-        row.reduce(
-          (acc, cell, j) => (cell === 'O' ? acc + 100 * i + j : acc),
-          acc,
-        ),
-      0,
-    ),
+const move2 = (matrix, [dx, dy]) => {
+  const positionX = matrix.findIndex(row => row.includes('@'));
+  const positionY = matrix[positionX].indexOf('@');
+  const nextX = positionX + dx;
+  const nextY = positionY + dy;
+  if (matrix[nextX][nextY] === '#') return;
+  if (!['[', ']'].includes(matrix[nextX][nextY])) {
+    return execute(matrix, [
+      { from: [positionX, positionY], to: [nextX, nextY] },
+    ]);
+  }
+
+  if (dx) {
+    const queue = [[nextX, nextY]];
+    const moves = [{ from: [positionX, positionY], to: [nextX, nextY] }];
+    const visited = [];
+    while (queue.length) {
+      const [x, y] = queue.shift();
+      if (matrix[x + dx][y] === '#') return;
+      if (visited.includes(`${[x, y]}`)) continue;
+      visited.push(`${[x, y]}`);
+      if (matrix[x][y] === '[') queue.push([x, y + 1]);
+      if (matrix[x][y] === ']') queue.push([x, y - 1]);
+      if (matrix[x + dx][y] !== '.') queue.push([x + dx, y]);
+      moves.unshift({ from: [x, y], to: [x + dx, y] });
+    }
+
+    return execute(matrix, moves);
+  }
+  const moves = [
+    { from: [nextX, nextY], to: [nextX, nextY + dy] },
+    { from: [positionX, positionY], to: [nextX, nextY] },
+  ];
+  while (['[', ']'].includes(matrix[moves[0].to[0]][moves[0].to[1]])) {
+    const [{ from, to }] = moves;
+    moves.unshift({ from: [from[0], from[1] + dy], to: [to[0], to[1] + dy] });
+  }
+  if (moves.some(({ to: [x, y] }) => matrix[x][y] === '#')) return;
+
+  return execute(matrix, moves);
+};
+
+const run =
+  fn =>
   ({ matrix, steps }) =>
-    steps.reduce(
-      (matrix, step) => move({ matrix, direction: directions[step] }) || matrix,
-      matrix,
+    steps.forEach(step => fn(matrix, directions[step])) || matrix;
+
+const part1 = compose(calculate('O'), run(move), prepare);
+
+const part2 = compose(
+  calculate('['),
+  run(move2),
+  evolve({
+    matrix: map(
+      chain(
+        cond([
+          [equals('O'), always(['[', ']'])],
+          [equals('@'), always(['@', '.'])],
+          [T, juxt([identity, identity])],
+        ]),
+      ),
     ),
+  }),
   prepare,
 );
 
-const part2 = compose(prepare);
-
 console.log('part 1', part1(data));
-// console.log('part 2', part2(data));
+console.log('part 2', part2(data));
