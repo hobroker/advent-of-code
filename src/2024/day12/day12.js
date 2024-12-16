d15cf27d4bf3a21c8b12822ea8fdb37aec9aa4c7
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2024-12-16 09:57:43 +0200

    2024 day 12 part 2

--- src/2024/day12/day12.js
+++ src/2024/day12/day12.js
@@ -1,15 +1,5 @@
 import { read } from '../../../lib/read.js';
-import {
-  add,
-  any,
-  applySpec,
-  compose,
-  identity,
-  map,
-  reduce,
-  split,
-  sum,
-} from 'ramda';
+import { add, any, applySpec, compose, identity, map, split, sum } from 'ramda';
 
 const data = read('input.txt');
 
@@ -27,6 +17,36 @@ const xyEquals =
   ([x2, y2]) =>
     x1 === x2 && y1 === y2;
 
+const identifyRegions = applySpec({
+  matrix: identity,
+  regions: matrix =>
+    matrix.reduce((regions, row, i) => {
+      row.forEach((block, j) => {
+        if (regions.some(any(xyEquals([i, j])))) return;
+
+        const plotsToExplore = [[i, j]];
+        const region = [[i, j]];
+        regions.push(region);
+
+        while (plotsToExplore.length) {
+          const plot = plotsToExplore.pop();
+          const [x, y] = plot;
+          directions.forEach(([dx, dy]) => {
+            const [newX, newY] = [x + dx, y + dy];
+            if (
+              matrix[newX]?.[newY] === block &&
+              !region.some(xyEquals([newX, newY]))
+            ) {
+              plotsToExplore.push([newX, newY]);
+              region.push([newX, newY]);
+            }
+          });
+        }
+      });
+      return regions;
+    }, []),
+});
+
 const perimeter = ({ region, matrix }) =>
   region
     .flatMap(([x, y]) =>
@@ -38,39 +58,52 @@ const part1 = compose(
   sum,
   ({ matrix, regions }) =>
     regions.map(region => region.length * perimeter({ region, matrix })),
-  applySpec({
-    matrix: identity,
-    regions: matrix =>
-      matrix.reduce((regions, row, i) => {
-        row.forEach((block, j) => {
-          if (regions.some(any(xyEquals([i, j])))) return;
-
-          const plotsToExplore = [[i, j]];
-          const region = [[i, j]];
-          regions.push(region);
-
-          while (plotsToExplore.length) {
-            const plot = plotsToExplore.pop();
-            const [x, y] = plot;
-            directions.forEach(([dx, dy]) => {
-              const [newX, newY] = [x + dx, y + dy];
-              if (
-                matrix[newX]?.[newY] === block &&
-                !region.some(xyEquals([newX, newY]))
-              ) {
-                plotsToExplore.push([newX, newY]);
-                region.push([newX, newY]);
-              }
-            });
-          }
-        });
-        return regions;
-      }, []),
-  }),
+  identifyRegions,
   prepare,
 );
 
-// const part2 = compose(prepare);
+const part2 = compose(
+  sum,
+  ({ matrix, regions }) =>
+    regions.map(region => {
+      const boundaries = region
+        .flatMap(([x, y]) =>
+          directions.map(([dx, dy]) =>
+            matrix[x][y] !== matrix[x + dx]?.[y + dy]
+              ? [
+                  [x, y],
+                  [x + dx, y + dy],
+                ]
+              : null,
+          ),
+        )
+        .filter(Boolean);
+
+      const perimeter = boundaries.filter(([[ix, iy], [jx, jy]]) => {
+        if (ix === jx) {
+          return !boundaries.some(
+            ([otherInner, otherOuter]) =>
+              ix + 1 === otherInner[0] &&
+              jx + 1 === otherOuter[0] &&
+              iy === otherInner[1] &&
+              jy === otherOuter[1],
+          );
+        }
+        if (iy === jy) {
+          return !boundaries.some(
+            ([otherInner, otherOuter]) =>
+              iy + 1 === otherInner[1] &&
+              jy + 1 === otherOuter[1] &&
+              ix === otherInner[0] &&
+              jx === otherOuter[0],
+          );
+        }
+      });
+      return region.length * perimeter.length;
+    }),
+  identifyRegions,
+  prepare,
+);
 
 console.log('part 1', part1(data));
-// console.log('part 2', part2(data));
+console.log('part 2', part2(data));
