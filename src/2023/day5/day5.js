2db853fc01de3fe7c5f3752c3b3cbd99ac3a4146
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2024-12-28 12:09:55 +0200

    2023 day 5 part 2

--- src/2023/day5/day5.js
+++ src/2023/day5/day5.js
@@ -1,4 +1,14 @@
-import { applySpec, compose, head, last, map, match, split, tail } from 'ramda';
+import {
+  applySpec,
+  compose,
+  evolve,
+  head,
+  last,
+  map,
+  match,
+  split,
+  tail,
+} from 'ramda';
 import { read } from '../../../lib/read.js';
 
 const data = read(`${import.meta.dirname}/input.txt`);
@@ -28,7 +38,44 @@ const part1 = compose(({ seeds, maps }) => {
   return Math.min(...seeds);
 }, prepare);
 
-// const part2 = compose(prepare);
+const part2 = compose(
+  ({ seeds, maps }) => {
+    let copy = [...seeds.map(seed => [...seed])];
+    for (let map of maps) {
+      const moved = [];
+      for (const [destination, source, length] of map) {
+        const unmoved = [];
+        for (const [start, end] of copy) {
+          if (start < source + length && end >= source) {
+            moved.push([
+              Math.max(start, source) - source + destination,
+              Math.min(end, source + length - 1) - source + destination,
+            ]);
+          }
+          if (start < source) {
+            unmoved.push([start, Math.min(end, source - 1)]);
+          }
+          if (end >= source + length) {
+            unmoved.push([Math.max(start, source + length), end]);
+          }
+        }
+        copy = unmoved;
+      }
+      copy.push(...moved);
+    }
+    return Math.min(...copy.flat());
+  },
+  evolve({
+    seeds: seeds => {
+      const nextSeeds = [];
+      for (let i = 0; i < seeds.length; i += 2) {
+        nextSeeds.push([seeds[i], seeds[i] + seeds[i + 1] - 1]);
+      }
+      return nextSeeds;
+    },
+  }),
+  prepare,
+);
 
 console.log('part 1', part1(data));
-// console.log('part 2', part2(data));
+console.log('part 2', part2(data));
