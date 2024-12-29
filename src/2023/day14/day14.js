ccd7387f3c1977e4de816d29f00c1cce9d2404c3
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2024-12-29 14:55:13 +0200

    2023 day 14 part 2

--- src/2023/day14/day14.js
+++ src/2023/day14/day14.js
@@ -1,31 +1,94 @@
-import { compose, equals, map, split, tap } from 'ramda';
+import { compose, equals, flatten, join, map, split, tap } from 'ramda';
 import { read } from '../../../lib/read.js';
 
 const data = read(`${import.meta.dirname}/input.txt`);
 const prepare = compose(map(split('')), split('\n'));
 
-const roll = (matrix, i, j) => {
+const rollCell = (direction, matrix, i, j) => {
   if (matrix[i][j] !== 'O') return;
-  while (matrix[i - 1]?.[j] === '.') {
-    [matrix[i][j], matrix[i - 1][j]] = [matrix[i - 1][j], matrix[i][j]];
-    i--;
+  let [toI, toJ] = [i, j];
+  while (matrix[toI + direction[0]]?.[toJ + direction[1]] === '.') {
+    toI += direction[0];
+    toJ += direction[1];
+  }
+
+  [matrix[i][j], matrix[toI][toJ]] = [matrix[toI][toJ], matrix[i][j]];
+};
+
+const roll = matrix => {
+  // roll North
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      rollCell([-1, 0], matrix, i, j);
+    }
+  }
+  // roll West
+  for (let j = 0; j < matrix.length; j++) {
+    for (let i = matrix.length - 1; i >= 0; i--) {
+      rollCell([0, -1], matrix, i, j);
+    }
+  }
+  // roll South
+  for (let i = matrix.length - 1; i >= 0; i--) {
+    for (let j = matrix.length - 1; j >= 0; j--) {
+      rollCell([1, 0], matrix, i, j);
+    }
+  }
+  // roll East
+  for (let j = matrix.length - 1; j >= 0; j--) {
+    for (let i = 0; i < matrix.length; i++) {
+      rollCell([0, 1], matrix, i, j);
+    }
   }
 };
 
+const countLoad = matrix =>
+  matrix.reduce(
+    (weight, row, i) =>
+      weight + row.filter(equals('O')).length * (matrix.length - i),
+    0,
+  );
+
+const matrixToString = compose(join(''), flatten);
+
 const part1 = compose(
-  matrix =>
-    matrix.reduce(
-      (weight, row, i) =>
-        weight + row.filter(equals('O')).length * (matrix.length - i),
-      0,
-    ),
+  countLoad,
   tap(matrix => {
-    matrix.forEach((row, i) => row.forEach((_, j) => roll(matrix, i, j)));
+    return matrix.forEach((row, i) =>
+      row.forEach((_, j) => {
+        rollCell([-1, 0], matrix, i, j);
+      }),
+    );
   }),
   prepare,
 );
 
-// const part2 = compose(prepare);
+const part2 = compose(
+  countLoad,
+  tap(matrix => {
+    let cycle = 0;
+    const visited = new Set();
+
+    while (true) {
+      cycle++;
+      roll(matrix);
+      const key = matrixToString(matrix);
+      if (visited.has(key)) break;
+      visited.add(key);
+    }
+    const periodicStates = new Set();
+    while (!periodicStates.has(matrixToString(matrix))) {
+      periodicStates.add(matrixToString(matrix));
+      roll(matrix);
+    }
+
+    const cyclesLeft = (1_000_000_000 - cycle) % periodicStates.size;
+    for (let i = 0; i < cyclesLeft; i++) {
+      roll(matrix);
+    }
+  }),
+  prepare,
+);
 
 console.log('part 1', part1(data));
-// console.log('part 2', part2(data));
+console.log('part 2', part2(data));
